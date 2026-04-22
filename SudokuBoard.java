6da3c9a96cf7f544a2bac4f7a1ebdea8258c09d1
hashirawais162-droid <hashirawais162@gmail.com>
2026-04-22 11:40:26 -0700

    Update SudokuBoard.java changes

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.util.Scanner;
+import java.util.HashSet;
 
 public class SudokuBoard {
 
@@ -40,4 +41,71 @@ public class SudokuBoard {
 
         return result;
     }
+
+
+public boolean isValid() {
+
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            char c = board[i][j];
+
+            if (!(c == '.' || (c >= '1' && c <= '9'))) {
+                return false;
+            }
+        }
+    }
+
+    for (int i = 0; i < 9; i++) {
+        HashSet<Character> set = new HashSet<>();
+
+        for (int j = 0; j < 9; j++) {
+            char c = board[i][j];
+
+            if (c != '.') {
+                if (set.contains(c)) {
+                    return false;
+                }
+                set.add(c);
+            }
+        }
+    }
+
+    for (int j = 0; j < 9; j++) {
+        HashSet<Character> set = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {
+            char c = board[i][j];
+
+            if (c != '.') {
+                if (set.contains(c)) {
+                    return false;
+                }
+                set.add(c);
+            }
+        }
+    }
+
+    for (int row = 0; row < 9; row += 3) {
+        for (int col = 0; col < 9; col += 3) {
+
+            HashSet<Character> set = new HashSet<>();
+
+            for (int i = row; i < row + 3; i++) {
+                for (int j = col; j < col + 3; j++) {
+
+                    char c = board[i][j];
+
+                    if (c != '.') {
+                        if (set.contains(c)) {
+                            return false;
+                        }
+                        set.add(c);
+                    }
+                }
+            }
+        }
+    }
+
+    return true;
+  }
 }
